c05c6a37aee712feccae71f548c2068457d0492b
taonad <you@example.com>
2024-04-14 06:35:43 +0700

    anh duy

--- btontintuc/script.js
+++ btontintuc/script.js
@@ -1,9 +1,28 @@
-function hideOrshow() {
-  var element = document.getElementById("menu");
+function hideOrshow(id) {
+  var element = document.getElementById(id);
   if (element.classList.contains("hidden")) {
     element.classList.replace("hidden", "block");
   } else {
     element.classList.replace("block", "hidden");
   }
 
+}
+
+function hideOrshow2() {
+  var element = document.getElementById('dropdown_menu');
+  if (element.classList.contains("hidden")) {
+    element.classList.replace("hidden", "block");
+  } else {
+    element.classList.replace("block", "hidden");
+  }
+
+}
+
+function toggleRadioList(id) {
+  var element = document.getElementById(id);
+  if (element.classList.contains("hidden")) {
+    element.classList.remove("hidden");
+  } else {
+    element.classList.add("hidden");
+  }
 }
